35233845ae0aee09972ae3314b1f9ad83d4d7020
unknown <hanaparsa_80@yahoo.com>
2024-01-30 14:42:18 +0330

    creating a sample house

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -22,10 +22,3 @@ import { HomeComponent } from './home/home.component';
 export class AppComponent {
   title = 'homes';
 }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
