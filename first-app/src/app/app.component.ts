08b2b647ceafd3690bc4bfd3eaa82897c2121cad
unknown <hanaparsa_80@yahoo.com>
2024-01-30 09:53:00 +0330

    in progress

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -1,26 +1,31 @@
-import { Component, NgModule } from '@angular/core';
-
+import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [
+    HomeComponent,
+  ],
   template: `
-  <main>
-    <header class="brand-name">
-    </header>
-    <section class="content">
-      <app-home></app-home>
-    </section>
-  </main>
-`,
-  imports: [ 
-    HomeComponent]
-  ,
-  styleUrl: './app.component.css'
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
+  styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
   title = 'homes';
 }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
--- first-app/src/app/home/home.component.ts
+++ first-app/src/app/home/home.component.ts
@@ -1,25 +1,30 @@
 import { Component } from '@angular/core';
-import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    HousingLocationComponent
+    CommonModule,
   ],
   template: `
-  <section>
-    <form>
-      <input type="text" placeholder="Filter by city">
-      <button class="primary" type="button">Search</button>
-    </form>
-  </section>
-  <section class="results">
-    <app-housing-location></app-housing-location>
-  </section>
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city">
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
   `,
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
 })
+
 export class HomeComponent {
 
 }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
