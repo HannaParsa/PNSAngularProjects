7218dd3bf430a66490831b92225a2ba5dac0dfc4
unknown <hanaparsa_80@yahoo.com>
2024-02-12 10:13:26 +0330

    add picture

--- first-app/src/app/housing-location/housing-location.component.ts
+++ first-app/src/app/housing-location/housing-location.component.ts
@@ -5,16 +5,19 @@ import { HousingLocation } from '../housinglocation';
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
-    <p>
-      housing-location works!
-    </p>
-  `,
-  styleUrl: './housing-location.component.css'
+    <section class="listing">
+      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+    </section>
+    `,
+  styleUrls: ['./housing-location.component.css'],
 })
 
-//it means it does not have default value
 export class HousingLocationComponent {
+
   @Input() housingLocation!: HousingLocation;
+
 }
