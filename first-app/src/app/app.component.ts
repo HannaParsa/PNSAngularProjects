21ed2df85b3a39556c2707dd5397fb675224c635
unknown <hanaparsa_80@yahoo.com>
2024-02-17 14:20:14 +0330

    in progress

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
