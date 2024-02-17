c6c922e272e04898ad92731f2ddfa467bf0df2d6
unknown <hanaparsa_80@yahoo.com>
2024-02-17 14:23:42 +0330

    in progress

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -7,17 +7,20 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [
     HomeComponent,
+    RouterModule,
   ],
   template: `
-    <main>
+  <main>
+    <a [routerLink]="['/']">
       <header class="brand-name">
         <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
       </header>
-      <section class="content">
-        <app-home></app-home>
-      </section>
-    </main>
-  `,
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
+`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
---
+++ first-app/src/app/routes.ts
@@ -0,0 +1,3 @@
+// Example: app.module.ts
+import { NgModule } from '@angular/core';
+// Other imports and module definition
