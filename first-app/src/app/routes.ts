4d55aff89a04ff612846383cb8f38a21473a38c4
unknown <hanaparsa_80@yahoo.com>
2024-02-17 14:36:07 +0330

    routing

--- first-app/src/app/routes.ts
+++ first-app/src/app/routes.ts
@@ -1,3 +1,17 @@
-// Example: app.module.ts
-import { NgModule } from '@angular/core';
-// Other imports and module definition
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+const routeConfig: Routes = [
+    {
+      path: '',
+      component: HomeComponent,
+      title: 'Home page'
+    },
+    {
+      path: 'details/:id',
+      component: DetailsComponent,
+      title: 'Home details'
+    }
+  ];
+  
+  export default routeConfig;
