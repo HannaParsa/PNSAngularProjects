7141664efcb2be2e1e1ac0c548f88bee1c57e648
unknown <hanaparsa_80@yahoo.com>
2024-02-17 12:54:31 +0330

    routing

--- first-app/src/main.ts
+++ first-app/src/main.ts
@@ -1,6 +1,15 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { provideRouter } from '@angular/router';
+import routeConfig from './app/routes';
+
+bootstrapApplication(AppComponent,
+  {
+    providers: [
+      provideProtractorTestingSupport(),
+      provideRouter(routeConfig)
+    ]
+  }
+).catch(err => console.error(err));
