bd0243419b417a29c45b5a7c63e85de12398d55e
unknown <hanaparsa_80@yahoo.com>
2024-02-17 15:22:11 +0330

    add dynamic links

--- first-app/src/app/housing-location/housing-location.component.ts
+++ first-app/src/app/housing-location/housing-location.component.ts
@@ -11,6 +11,7 @@ import { HousingLocation } from '../housinglocation';
       <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
       <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
     </section>
     `,
   styleUrls: ['./housing-location.component.css'],
